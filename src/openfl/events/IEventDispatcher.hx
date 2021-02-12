e89cc40d4fc6e20326bebf200d8c7801967a3408
Juraj Kirchheim <back2dos@gmail.com>
2021-02-12 17:52:06 +0100

    Actually make use of EventType.

--- src/openfl/events/IEventDispatcher.hx
+++ src/openfl/events/IEventDispatcher.hx
@@ -1,9 +1,9 @@
 package openfl.events;
 
 interface IEventDispatcher {
-	public function addEventListener<T>(type:EventType<T>, listener:Dynamic->Void, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void;
+	public function addEventListener<T>(type:EventType<T>, listener:T->Void, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void;
 	public function dispatchEvent(event:Event):Bool;
 	public function hasEventListener(type:String):Bool;
-	public function removeEventListener<T>(type:EventType<T>, listener:Dynamic->Void, useCapture:Bool = false):Void;
+	public function removeEventListener<T>(type:EventType<T>, listener:T->Void, useCapture:Bool = false):Void;
 	public function willTrigger(type:String):Bool;
 }
